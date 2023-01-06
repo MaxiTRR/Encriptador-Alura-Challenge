35702cac3080509d12ba4b5eda5651e37f016f04
MaxiTRR <maxihtorres@gmail.com>
2023-01-06 19:08:53 -0300

    Finalizacion de la logica del Encriptador (Boton copiar)

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let textoAEncriptar = document.querySelector('input');
 let textoDesencriptado = document.querySelector('.desencriptador');
+let btnCopiar = document.querySelector('#btn-copiar');
+let descImg = document.querySelector('#desc-img');
+let descTitle = document.querySelector('#des-title');
+let textShow = document.querySelector('#textShow');
+
+btnCopiar.style.display = 'none';
 const dicc = {
     'a':'enter',
     'e':'jojo',
@@ -23,7 +29,6 @@ function encriptar(texto){
     console.log(textoEncriptado);
     return textoEncriptado;
     //let resultado = textoAEncriptar.value;
-
 };
 
 function desencriptador(texto){
@@ -34,18 +39,48 @@ function desencriptador(texto){
                          .replaceAll('poco', 'i')
                          .replaceAll('kara', 'o')
                          .replaceAll('kura', 'u')
+        
+    } else {
+        descImg.style.display = 'block';
+        descTitle.style.display = 'block';
     }
     return textoReverso;
 }   
 
 let buttonEnc = document.querySelector('#btn-encriptador');
 buttonEnc.addEventListener('click',()=>{
-    textoDesencriptado.innerText = encriptar(textoAEncriptar.value);
-    ;
+    if(textoAEncriptar.value !== ''){
+        textShow.innerText = encriptar(textoAEncriptar.value);
+        descImg.style.display = 'none';
+        descTitle.style.display = 'none';
+        btnCopiar.style.display = 'block';
+    }else{
+        descImg.style.display = 'block';
+        descTitle.style.display = 'block';
+        btnCopiar.style.display = 'none';
+    }; 
 });
+
+
 
 let buttonDesc = document.querySelector('#btn-desencriptador');
 buttonDesc.addEventListener('click',()=>{
-    textoDesencriptado.innerText = desencriptador(textoAEncriptar.value);
-    console.log('Se tiene que ver esto')
+    if(textoAEncriptar.value !== ''){
+        textShow.innerText = desencriptador(textShow.innerText);
+    //console.log(textoAEncriptar.value)
+    };
 });
+
+function copyTexto(htmlElement) {
+    let copyText = htmlElement.innerText
+    let inputElement = document.createElement('input');
+    inputElement.setAttribute('value', copyText);
+    document.body.appendChild(inputElement)
+    inputElement.select();
+    document.execCommand("copy");
+    inputElement.style.display = 'none';
+  }
+  
+btnCopiar.onclick = function(){
+    copyTexto(textShow)
+}
